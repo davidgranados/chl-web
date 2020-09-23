13b427204474a8301545b228e00e7717a5f56d53
David Granados <david.granados@aguayo.co>
2020-09-23 00:19:57 -0500

    add models to admin

--- chl_web/orders/admin.py
+++ chl_web/orders/admin.py
@@ -1,3 +1,9 @@
+"""Orders admin setup."""
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
